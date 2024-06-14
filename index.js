01af9656f644588600f83e3a19126e3e1aef19d5
Víctor Fernández <vicbolt.madrid@gmail.com>
2024-06-14 20:49:47 +0200

    index

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 'use strict';
 
 var mongoose = require('mongoose');
-// const cors = require('cors');
+const cors = require('cors');
 
 var app = require('./app')
 // var port = 4400;
 
-// app.use(cors({
-//     origin: '*', // Permite solicitudes desde este origen
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
-//     allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
-//     credentials: true // Permite cookies y encabezados de autorización
-// }));
+app.use(cors({
+    origin: '*', // Permite solicitudes desde este origen
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+    credentials: true // Permite cookies y encabezados de autorización
+}));
 
-// mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb+srv://vicboltmadrid:IJXgDWBNSPQBhDcJ@rosanadb.mtxmv1m.mongodb.net/?retryWrites=true&w=majority&appName=RosanaDB', { useNewUrlParser: true }).then(() =>{
     console.log('Success! DB Connected')
