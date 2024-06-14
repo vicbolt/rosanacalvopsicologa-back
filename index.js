dcc67ec494d8045681faae9e52dce83e9d2cd5af
Víctor Fernández <vicbolt.madrid@gmail.com>
2024-06-14 20:53:30 +0200

    url en variable

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 const cors = require('cors');
 
 var app = require('./app')
-// var port = 4400;
+var url = 'mongodb+srv://vicboltmadrid:IJXgDWBNSPQBhDcJ@rosanadb.mtxmv1m.mongodb.net/?retryWrites=true&w=majority&appName=RosanaDB';
 
 app.use(cors({
     origin: '*', // Permite solicitudes desde este origen
@@ -15,11 +15,11 @@ app.use(cors({
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb+srv://vicboltmadrid:IJXgDWBNSPQBhDcJ@rosanadb.mtxmv1m.mongodb.net/?retryWrites=true&w=majority&appName=RosanaDB', { useNewUrlParser: true }).then(() =>{
+mongoose.connect(url, { useNewUrlParser: true }).then(() =>{
     console.log('Success! DB Connected')
 
     app.listen(() =>{
-        console.log('Server works!')
+        console.log('DB working in: ' + url)
     })
 })
 
